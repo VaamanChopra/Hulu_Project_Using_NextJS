1c2450ef8ac1f68d457d06a53fc2b6f7c344a22c
VaamanChopra <vaamanchopra@gmail.com>
2023-12-19 12:20:21 +0530

    Tailwind edits

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,7 +1,19 @@
+import Image from "next/image"
+
 function Thumbnail({ result }) {
+    const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
-    <div>
-        <h1>abc</h1>
+    <div >
+        <Image
+              layout="responsive"
+              src={
+                `${BASE_URL}${result.backdrop_path || 
+                 result.poster_path}` ||
+                `${BASE_URL}${result.poster_path}`
+              }
+              height={1080}
+              width={1920}
+        />
     </div>
   )
 }
