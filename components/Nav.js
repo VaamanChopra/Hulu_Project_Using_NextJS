d14a9f06bd1e703ac653671aa76291dfbf849dad
VaamanChopra <vaamanchopra@gmail.com>
2024-07-03 02:17:25 +0530

    Nav Bar

--- components/Nav.js
+++ components/Nav.js
@@ -3,7 +3,7 @@ import requests from "../utils/requests";
 function Nav() {
     console.log(requests)
   return (
-  <nav>
+  <nav className="relative">
     <div className=" flex px-10 sm: px-20 text-2xl
     whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
         {Object.entries(requests).map(([ key, { title, url }]) =>(
@@ -15,6 +15,7 @@ function Nav() {
             </h2>
         ))}
     </div>
+    <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" />
   </nav>
   );
 }
